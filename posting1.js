a44900408925f11c2483002fa261cf99149c3a92
fajeros90 <40035304+fajeros90@users.noreply.github.com>
2018-06-07 23:55:05 +0700

    Update posting1.js

--- posting1.js
+++ posting1.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="aku-cinta.html"}
-if(x=="2"){xxx="aku-cinta-kamu.html"}
-if(x=="3"){xxx="aku-cinta-kamu-dia.html"}
-if(x=="4"){xxx="aku-suka-kamu.html"}
-if(x=="5"){xxx="aku-suka-dia.html"}a_to_ve[i].href="https://siotong2.blogspot.com/2018/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"n";}}
+if(x=="1"){xxx="basic-principles-to-insurance.html"}
+if(x=="2"){xxx="basic-principles-to-insurance.html"}
+if(x=="3"){xxx="basic-principles-to-insurance.html"}
+if(x=="4"){xxx="basic-principles-to-insurance.html"}
+if(x=="5"){xxx="basic-principles-to-insurance.html"}a_to_ve[i].href="https://secretautoinsurance.blogspot.com/2014/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
